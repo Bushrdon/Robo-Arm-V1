f0b7234c68d0380ec82fa705e0597fecc2c7e3c1
Bushrdon <martinacontrerasraeb@gmail.com>
2025-12-15 15:26:44 -0400

    Anadido README y otras carpetas

---
+++ README.md
@@ -0,0 +1,35 @@
+# Read Me  
+
+## Overview:  
+
+ Robot Angular con 3 GDL controlado por medio de una interfaz grafica en Python/Tkinter y un microcontrolador ESP32 mediante motores paso a paso. 
+---
+
+## Features:  
+
+ El proyecto integra control directo de los motores paso a paso mediante una GUI simple ademas de una serie de funciones presentadas como botones extra para ejecutar patrones de movimiento especificos.  
+ El robot tambien posee elementos terminales intercambeables destinados a demostrar la utilidad de los patrones de movimiento integrados en la GUI.  
+ Funciones como Control de Velocidad por sensores y mecanismo complejos de transmision aun estan en contemplacion.  
+ 
+---
+
+## Prerequisites:  
+
+ * Hardware: 
+   1. Placa Espressif ESP32
+   2. Motores Paso a Paso
+   3. Cable Mini-USB
+   
+ * Software:
+   1. Arduino IDE
+   1. Python 3.x
+   
+ * Pip Packages:
+   2. Pillow 
+   3. PySerial
+	
+---
+
+## Installation/Setup:
+
+[Bajo desarrollo]
---
+++ Software/GUI/src/GUI.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import tkinter as tk
+
+import os
+from pathlib import Path
+
+from PIL import ImageTk, Image
+
+# Define Path for GUI images
+
+SRC = Path('D:/DATA/Desktop/RoboArm/Software/GUI/src/GUI.py').resolve().parent #Esto sera un problema con el tema de la redistribucion 
+GUI = SRC.parent
+IMG_DIR = GUI /"img"
+
+UP_ARROW = IMG_DIR /"up-arrow.png"
+LEFT_ARROW = IMG_DIR /"left-arrow.png"
+DOWN_ARROW = IMG_DIR /"down-arrow.png"
+ARROW = IMG_DIR /"arrow.png"
+RIGHT_ARROW = IMG_DIR /"right-arrow.png"
+
+
+# GUI Composition (using pillow)
+root = Tk()
+root.title("Robot Arm")
+
+up_arrow = ImageTk.PhotoImage(Image.open(UP_ARROW))
+btn = tk.Button(root)
+btn.grid(column=2, row=1, padx=2, pady=2)
+btn['image']=up_arrow
+
+left_arrow = ImageTk.PhotoImage(Image.open(LEFT_ARROW))
+btn1= tk.Button(root)
+btn1.grid(column=1, row=2, padx=2, pady=2)
+btn1['image']=left_arrow
+
+down_arrow = ImageTk.PhotoImage(Image.open(DOWN_ARROW))
+btn2= tk.Button(root)
+btn2.grid(column=2, row=3, padx=2, pady=2)
+btn2['image']=down_arrow
+
+arrow = ImageTk.PhotoImage(Image.open(ARROW))
+btn3= tk.Button(root)
+btn3.grid(column=2, row=2, padx=2, pady=2)
+btn3['image']=arrow
+
+right_arrow = ImageTk.PhotoImage(Image.open(RIGHT_ARROW))
+btn4= tk.Button(root)
+btn4.grid(column=3, row=2, padx=2, pady=2)
+btn4['image']=right_arrow
+
+#we would need to include some image difference explaining the movement produced by each button
+
+
+root.geometry("600x400")
