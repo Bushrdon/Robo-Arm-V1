82f5758389a011b5e54785c78001c87676166bcf
Bushrdon <martinacontrerasraeb@gmail.com>
2026-01-30 23:30:55 -0400

    done serial backend

--- Software/main/src/main.py
+++ Software/main/src/main.py
@@ -1,3 +1,4 @@
+
 from tkinter import *
 import tkinter as tk
 
@@ -6,26 +7,34 @@ from pathlib import Path
 
 from PIL import ImageTk, Image
 
+import serial
+
+# Initiate COM and Configure
+
+ser = serial.Serial('COM3', 9600, timeout=1)
+
+if not ser.is_open:
+    ser.open()
 
 # Define Button Commands
 
-#def MOVE_UP_1:
-#    ser.write(b'q')
+def MOVE_UP_1():
+    ser.write(b'q')
 
-#def MOVE_DOWN_1:
-#    ser.write(b'w')
+def MOVE_DOWN_1():
+    ser.write(b'w')
 
-#def MOVE_UP_2:
-#    ser.write(b'e')
+def MOVE_UP_2():
+    ser.write(b'e')
 
-#def MOVE_DOWN_2:
-#    ser.write(b'r')
+def MOVE_DOWN_2():
+    ser.write(b'r')
 
-#def ROTATE_RIGHT:
-#    ser.write(b's')
+def ROTATE_RIGHT():
+    ser.write(b's')
 
-#def ROTATE_LEFT:
-#    ser.write(b't')
+def ROTATE_LEFT():
+    ser.write(b't')
 
 # Define Path for GUI images
 
@@ -47,12 +56,12 @@ root = Tk()
 root.title("Robot Arm")
 
 up_arrow_1 = ImageTk.PhotoImage(Image.open(UP_ARROW))
-btn = tk.Button(root)
+btn = tk.Button(root, command = MOVE_UP_1)
 btn.grid(column=2, row=1, padx=2, pady=2)
 btn['image']=up_arrow_1
 
 down_arrow_1 = ImageTk.PhotoImage(Image.open(DOWN_ARROW))
-btn2= tk.Button(root)
+btn2= tk.Button(root, command = MOVE_DOWN_1)
 btn2.grid(column=2, row=3, padx=2, pady=2)
 btn2['image']=down_arrow_1
 
@@ -62,12 +71,12 @@ label1.grid(column=2, row=2, padx=2, pady=2)
 label1['image'] = joint1
 
 up_arrow_2 = ImageTk.PhotoImage(Image.open(UP_ARROW))
-btn3 = tk.Button(root)
+btn3 = tk.Button(root, command = MOVE_UP_2)
 btn3.grid(column=4, row=1, padx=2, pady=2)
 btn3['image']=up_arrow_2
 
 down_arrow_2 = ImageTk.PhotoImage(Image.open(DOWN_ARROW))
-btn4= tk.Button(root)
+btn4= tk.Button(root, command = MOVE_DOWN_2)
 btn4.grid(column=4, row=3, padx=2, pady=2)
 btn4['image']=down_arrow_2
 
@@ -77,12 +86,12 @@ label2.grid(column=4, row=2, padx=2, pady=2)
 label2['image'] = joint2
 
 right_arrow = ImageTk.PhotoImage(Image.open(RIGHT_ARROW))
-btn5= tk.Button(root)
+btn5= tk.Button(root, command = ROTATE_RIGHT)
 btn5.grid(column=7, row=2, padx=2, pady=2)
 btn5['image']=right_arrow
 
 left_arrow = ImageTk.PhotoImage(Image.open(LEFT_ARROW))
-btn6= tk.Button(root)
+btn6= tk.Button(root, command = ROTATE_LEFT)
 btn6.grid(column=5, row=2, padx=2, pady=2)
 btn6['image']=left_arrow
 
