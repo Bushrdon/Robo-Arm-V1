9c65f3e24c4a14264cfc8fcc9e368d36eca8c907
Bushrdon <martinacontrerasraeb@gmail.com>
2026-02-01 23:28:50 -0400

    added test file with servo functions

--- Firmware/src/test.ino
+++ Firmware/src/test.ino
@@ -1,9 +1,11 @@
 #include <AccelStepper.h>
 #include <Arduino.h>
+#include <Servo.h>
 
 // Used by testing Arduino Code in Arduino UNO R3 in Proteus
 // It can technically be run by a Arduino UNO if you follow the pin definitions below. 
 
+//Steppers 
 
 #define JOINT1_1 9
 #define JOINT1_2 8
@@ -20,10 +22,27 @@
 #define JOINT3_3 A2
 #define JOINT3_4 A3
 
-
 AccelStepper joint1(8, JOINT1_1, JOINT1_3, JOINT1_2, JOINT1_4); //Shoulder
 AccelStepper joint2(8, JOINT2_1, JOINT2_3, JOINT2_2, JOINT2_4); //Elbow
 AccelStepper joint3(8, JOINT3_1, JOINT3_3, JOINT3_2, JOINT3_4); //Wrist
+
+//Servos
+
+#define PINZA_1 A4
+#define PINZA_2 A5
+
+Servo servo1;
+Servo servo2;
+
+//Variables
+
+int steps = 90;
+int POS = 90;
+
+//Functions
+
+void open_clamp(int target);
+void close_clamp(int target);
 
 void setup()
 {     
@@ -35,39 +54,76 @@ void setup()
    joint2.setSpeed(10);
    
    joint3.setMaxSpeed(100);
-   joint3.setSpeed(10);          
+   joint3.setSpeed(10);
+
+   servo1.write(POS);
+   servo2.write(POS);
+   servo1.attach(PINZA_1);          
+   servo2.attach(PINZA_2);
 }
 
-void loop() {  
+void loop() {   
 
   if(Serial.available()>0){
-
+    int target = POS;
     char UserInput = Serial.read();
-    
+
     if(UserInput == 't'){
-      joint1.moveTo(50);
-      Serial.print("Received 1... Moving Joint 1 (50 steps)\n");
+      joint1.moveTo(steps);
+      Serial.print("Received 1... Moving Joint 1\n");
       joint1.run();
     } else if (UserInput == 's'){
-      joint1.moveTo(-50);
-      Serial.print("Received 2... Moving Joint 1 (-50 steps)\n");
+      joint1.moveTo(-steps);
+      Serial.print("Received 2... Moving Joint 1\n");
       joint1.run();
     } else if (UserInput == 'e'){
-      joint2.moveTo(50);
-      Serial.print("Received 2... Moving Joint 2 (50 steps)\n");
+      joint2.moveTo(steps);
+      Serial.print("Received 2... Moving Joint 2\n");
       joint2.run();
     } else if (UserInput == 'r'){
-      joint2.moveTo(-50);
-      Serial.print("Received 2... Moving Joint 2 (-50 steps)\n");
+      joint2.moveTo(-steps);
+      Serial.print("Received 2... Moving Joint 2\n");
       joint2.run();
     } else if (UserInput == 'q'){
-      joint3.moveTo(50);
-      Serial.print("Received 3... Moving Joint 3 (50 steps)\n");
+      joint3.moveTo(steps);
+      Serial.print("Received 3... Moving Joint 3\n");
       joint3.run();
     } else if (UserInput == 'w'){
-      joint3.moveTo(-50);
-      Serial.print("Received 2... Moving Joint 3 (-50 steps)\n");
-      joint2.run();
+      joint3.moveTo(-steps);
+      Serial.print("Received 2... Moving Joint 3\n");
+      joint3.run();
+    } else if(UserInput == 'a'){
+      open_clamp(target);
+    } else if(UserInput == 'm'){
+      close_clamp(target);
     }
-   } 
+   }
+}
+
+void open_clamp(int target){
+      target = 110;
+      while(POS != target){
+      if(POS < target){
+        POS++;
+      } else {
+        POS--;
+      }
+    servo1.write(target);
+    delay(20);
+    servo1.write(90);
+    }
+}
+
+void close_clamp(int target){
+      target = 70;
+      while(POS != target){
+      if(POS < target){
+        POS++;
+      } else {
+        POS--;
+      }
+    servo1.write(target);
+    delay(20);
+    servo1.write(90);
+    }
 }
