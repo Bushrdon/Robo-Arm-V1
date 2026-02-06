6f3c5e5c08b784d281f2d1e15d2fe4d64ba8824e
Bushrdon <martinacontrerasraeb@gmail.com>
2026-02-05 23:37:52 -0400

    Added Joint 4

--- Firmware/src/test.ino
+++ Firmware/src/test.ino
@@ -2,8 +2,7 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-// Used by testing Arduino Code in Arduino UNO R3 in Proteus
-// It can technically be run by a Arduino UNO if you follow the pin definitions below. 
+// Arduino Test Code
 
 //Steppers 
 
@@ -29,24 +28,24 @@ AccelStepper joint3(8, JOINT3_1, JOINT3_3, JOINT3_2, JOINT3_4); //Wrist
 //Servos
 
 #define PINZA_1 A4
-#define PINZA_2 A5
+#define JOINT4 A5
 
-Servo servo1;
-Servo servo2;
+Servo servo1; // Clamp
+Servo joint4; //4th Joint
 
 //Variables
 
-int steps = 90;
+int steps = 10;
 int POS = 90;
-
 //Functions
 
-void open_clamp(int target);
-void close_clamp(int target);
+void open_clamp(int target_1);
+void close_clamp(int target_1);
 
 void setup()
 {     
    Serial.begin(9600);
+
    joint1.setMaxSpeed(100);
    joint1.setSpeed(10);
    
@@ -57,73 +56,117 @@ void setup()
    joint3.setSpeed(10);
 
    servo1.write(POS);
-   servo2.write(POS);
+  joint4.write(POS);
    servo1.attach(PINZA_1);          
-   servo2.attach(PINZA_2);
+  joint4.attach(JOINT4);
 }
 
 void loop() {   
 
   if(Serial.available()>0){
-    int target = POS;
+    int target_1 = POS;
+    int target_2 = POS;
     char UserInput = Serial.read();
 
     if(UserInput == 't'){
       joint1.moveTo(steps);
-      Serial.print("Received 1... Moving Joint 1\n");
+      Serial.print("Moving Joint 1 +\n");
       joint1.run();
+      joint1.stop();
+
     } else if (UserInput == 's'){
       joint1.moveTo(-steps);
-      Serial.print("Received 2... Moving Joint 1\n");
+      Serial.print("Moving Joint 1 -\n");
       joint1.run();
+      joint1.stop();
     } else if (UserInput == 'e'){
       joint2.moveTo(steps);
-      Serial.print("Received 2... Moving Joint 2\n");
+      Serial.print("Moving Joint 2 +\n");
       joint2.run();
+      joint2.stop();
     } else if (UserInput == 'r'){
       joint2.moveTo(-steps);
-      Serial.print("Received 2... Moving Joint 2\n");
+      Serial.print("Moving Joint 2 -\n");
       joint2.run();
+      joint2.stop();
     } else if (UserInput == 'q'){
       joint3.moveTo(steps);
-      Serial.print("Received 3... Moving Joint 3\n");
+      Serial.print("Moving Joint 3 +\n");
       joint3.run();
+      joint3.stop();
     } else if (UserInput == 'w'){
       joint3.moveTo(-steps);
-      Serial.print("Received 2... Moving Joint 3\n");
+      Serial.print("Moving Joint 3 -\n");
       joint3.run();
+      joint3.stop();
     } else if(UserInput == 'a'){
-      open_clamp(target);
+      Serial.print("Opening Clamp \n");
+      open_clamp(target_1);
     } else if(UserInput == 'm'){
-      close_clamp(target);
+      Serial.print("Closing Clamp \n");
+      close_clamp(target_1);
+    } else if(UserInput == 'o'){
+      Serial.print("Elevating Clamp\n");
+      move_upwards(target_2);
+    } else if (UserInput == 'p'){
+      Serial.print("Descending Clamp\n");
+      move_downwards(target_2);
     }
    }
 }
 
-void open_clamp(int target){
-      target = 110;
-      while(POS != target){
-      if(POS < target){
+void open_clamp(int target_1){
+      target_1 = 110;
+      while(POS != target_1){
+      if(POS < target_1){
         POS++;
       } else {
         POS--;
       }
-    servo1.write(target);
+    servo1.write(target_1);
     delay(20);
     servo1.write(90);
     }
 }
 
-void close_clamp(int target){
-      target = 70;
-      while(POS != target){
-      if(POS < target){
+void close_clamp(int target_1){
+      target_1 = 70;
+      while(POS != target_1){
+      if(POS < target_1){
         POS++;
       } else {
         POS--;
       }
-    servo1.write(target);
+    servo1.write(target_1);
     delay(20);
     servo1.write(90);
     }
+}
+
+void move_upwards(int target_2){
+  target_2 = 110;
+  while(POS != target_2){
+    if(POS < target_2){
+      POS++;
+    } else {
+      POS--;
+    }
+  joint4.write(target_2);
+  delay(20);
+  joint4.write(90);
+  }
+}
+
+void move_downwards(int target_2){
+  target_2 = 70;
+  while(POS != target_2){
+    if(POS < target_2){
+      POS++;
+    } else {
+      POS--;
+    }
+  joint4.write(target_2);
+  delay(20);
+  joint4.write(90);
+  }
 }
